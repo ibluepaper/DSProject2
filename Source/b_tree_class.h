6b93435552b80850cff88fd680890dfad7e2ba95
ibluepaper <alirezagh110@gmail.com>
2020-07-18 19:31:35 +0430

    insert function for empty tree modified

--- Source/b_tree_class.h
+++ Source/b_tree_class.h
@@ -10,4 +10,14 @@ public:
     this->t = t;
     root = nullptr;
   }
+
+  void insert(int a);
 };
+
+void BTree::insert(int a){
+  if (!root){
+    root = new BTreeNode(t, true);
+    root->setKeys(0, a);
+    root->setN(1);
+  }
+}
