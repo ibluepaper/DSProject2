b8271502757767d73c5868034d869cf91bdac215
ibluepaper <alirezagh110@gmail.com>
2020-07-19 16:41:18 +0430

    main.cpp is changed (not important)

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,19 +1,6 @@
-#include "b_tree_class.h"
+#include "hash_table_class.h"
 
 int main(){
-  BTree *bt = new BTree(3);
-  bt->insert(11);
-  bt->insert(21);
-  bt->insert(31);
-  bt->insert(61);
-  bt->insert(51);
-  bt->insert(41);
-  bt->insert(71);
-  bt->insert(91);
-  bt->insert(81);
 
-  BTreeNode *root = bt->getRoot();
-
-  cout << root->getChilds(0)->getKeys(0);
 
 }
