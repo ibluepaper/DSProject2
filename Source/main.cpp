fad432099e2db2a952f7f08cfffb251249f4b989
ibluepaper <alirezagh110@gmail.com>
2020-07-20 19:25:27 +0430

    stringToInt function modified

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,15 @@
 #include "hash_table_class.h"
+#include <string>
+
+int stringToInt(string str){
+    int len = str.size();
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+      sum += int(str[i]);
+    return sum;
+}
 
 int main(){
-
+  
 
 }
